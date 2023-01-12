5265b5e30346ac61814bd5cca31ab031f6b6e36e
WillFerra <91874103+WillFerra@users.noreply.github.com>
2023-01-12 15:14:08 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -60,3 +60,23 @@ function isShort(value){
         return false;
     }
 }
+
+// this is for the counter on the products page
+
+function increaseCount(a, b) {
+    var input = b.previousElementSibling;
+    var value = parseInt(input.value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    input.value = value;
+}
+
+function decreaseCount(a, b) {
+    var input = b.nextElementSibling;
+    var value = parseInt(input.value, 10);
+    if (value > 1) {
+        value = isNaN(value) ? 0 : value;
+        value--;
+        input.value = value;
+    }
+}
